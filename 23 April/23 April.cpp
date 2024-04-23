ba4f752f6e0fa330d0c443cd34141ac50e3f289e
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-23 09:47:23 +0700

    Prosedur output pada class Mahasiswa

--- 23 April/23 April.cpp
+++ 23 April/23 April.cpp
@@ -8,7 +8,10 @@ class Mahasiswa {
 		string jurusan;
 
 		void output() {
-		
+			cout << "Nama: " << nama << endl;
+			cout << "Umur: " << umur << endl;
+			cout << "Jurusan: " << jurusan << endl;
+
 		}
 };
 
